d0d0c2f21182af2da2d87b96518ef59d76a6c0e5
Matt Forster <forster.matth@gmail.com>
2015-04-29 04:48:30 +0000

    logs

--- app/init.js
+++ app/init.js
@@ -66,11 +66,17 @@ module.exports = {
   inspect: function () {
 
     redisClient.lrange(moment().isoWeek(), 0, -1, function (err, reply) {
-      if (err) console.log(err);
+      if (err) {
+        console.log('week list error: ');
+        console.log(err);
+      }
       console.log('this weeks list: ' + reply);
     });
     redisClient.get('tkey', function (err, reply) {
-      if (err) console.log(err);
+      if (err) {
+        console.log('transaction key error: ');
+        console.log(err);
+      }
       console.log('current transaction key: ' + reply);
     });
 
