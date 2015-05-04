0b0156df7dd1d1fcb0057092c82366c7a9c1bc6f
forstermatth <forster.matth@gmail.com>
2015-05-04 00:30:40 -0600

    check for sunday

--- s-task.js
+++ s-task.js
@@ -1,7 +1,13 @@
-var request = require('request');
+var request = require('request'),
+  moment = require('moment');
 
 var url = "https://autovance-fee-api.herokuapp.com/events/report?email=true";
-request(url, function(err, response, body){
-  if (err) console.log(err);
-  console.log(response.statusCode);
-});
+
+if (moment().isoWeekday() === 7) {
+  request(url, function(err, response, body){
+    if (err) console.log(err);
+    console.log(response.statusCode);
+  });
+} else {
+  console.log('no request');
+}
