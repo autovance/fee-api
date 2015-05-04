585e1c71de40149a0bcdbbdd1ba6dff977723fdc
forstermatth <forster.matth@gmail.com>
2015-05-04 00:00:17 -0600

    inline styles?

--- app/routes.js
+++ app/routes.js
@@ -62,7 +62,7 @@ module.exports = {
                   description: 'Here is a list of all of the fee\'s that were collected this week.',
                   vendor: 'Stripe Payments Platform',
                   list: slist.transactions,
-                  total: resp
+                  total: (resp/100).toFixed(2)
                 }
               });
 
